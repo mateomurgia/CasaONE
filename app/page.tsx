28f0adf3c6945115ac1a3a759770e2a8f6a0ea6b
Mateo <murgiamateo@gmail.com>
2026-05-15 09:19:39 -0300

    Use default system cursor

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import { Territories } from "@/components/Territories";
 import { Journey } from "@/components/Journey";
 import { Contact } from "@/components/Contact";
 import { ScrollProgress } from "@/components/ScrollProgress";
-import { CustomCursor } from "@/components/CustomCursor";
 import { SectionDivider, SectionDividerReverse } from "@/components/SectionDivider";
 
 export default function Home() {
   return (
     <>
       <ScrollProgress />
-      <CustomCursor />
       <main>
         <Hero />
         <SectionDivider />
